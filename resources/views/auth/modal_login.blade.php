d6ceb5233ca9c40465e9a46db2ae5d20e1c49ea6
Nikolay <nikolay.m@filancy.com>
2019-12-30 17:56:00 +0200

    translate

--- resources/views/auth/modal_login.blade.php
+++ resources/views/auth/modal_login.blade.php
@@ -61,7 +61,7 @@
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link text-white" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        {{ __('Forgot Your Password') . '?' }}
                                     </a>
                                 @endif
                             </div>
