9ad2dc06d3762812a0df2c6dc2804e12b125aad6
Nikolay <nikolay.m@filancy.com>
2020-01-09 14:48:24 +0200

    toggle language 6

--- resources/views/locales/locale.blade.php
+++ resources/views/locales/locale.blade.php
@@ -1,32 +1,19 @@
-{{-- <p> --}}
-	{{-- <select id="select-language" name="language"> --}}
-		{{-- @foreach (config('languages.languages') as $lang) --}}
-			{{-- <option {{ \App::getLocale() == $lang ? 'selected' : null }}>{{ $lang }}</option> --}}
-		{{-- @endforeach --}}
-	{{-- </select> --}}
 <div class="dropdown">
     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-      Dropdown button
+		<span class="flag-icon flag-icon-{{ \App::getLocale() }}"></span>
     </a>
     <div class="dropdown-menu">
-		<i class="dropdown-item" href="#">Link 1</i>
-		<i class="dropdown-item" href="#">Link 2</i>
-        <i class="dropdown-item" href="#">Link 3</i>
+		@foreach (config('languages.languages') as $lang)
+			<a class="dropdown-item" href="#" onclick="selectLanguage(this); event.preventDefault()" data-lang="{{ $lang }}"><span class="flag-icon flag-icon-{{ $lang }}"></span></a>
+		@endforeach
     </div>
 </div>
-{{-- </p> --}}
-<a href="#"><span class="flag-icon flag-icon-en"></span></a>
-<a href="#"><span class="flag-icon flag-icon-ru"></span></a>
-<a href="#"><span class="flag-icon flag-icon-uk"></span></a>
-
-
 
 <script>
 // change language
-{{-- let selectLanguage = document.querySelector('#select-language'); --}}
-{{-- selectLanguage.onchange = function(e) { --}}
-	{{-- let lang = this.value; --}}
-	{{-- let route = "{{ route('setlocale') }}"; --}}
-	{{-- document.location.href = route + '/' + lang + '/'; --}}
-{{-- }  --}}
+function selectLanguage(e) {
+	let lang = e.dataset.lang;
+	let route = "{{ route('setlocale') }}";
+	document.location.href = route + '/' + lang + '/';
+}
 </script>
