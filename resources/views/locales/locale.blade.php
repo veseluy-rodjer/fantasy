c67282e83949fd0b42ec523083a051a10891ef57
Nikolay <nikolay.m@filancy.com>
2020-01-09 11:48:55 +0200

    toggle language 5

--- resources/views/locales/locale.blade.php
+++ resources/views/locales/locale.blade.php
@@ -1,19 +1,32 @@
-<p>
-	<select id="select-language" name="language">
-		@foreach (config('languages.languages') as $lang)
-			<option {{ \App::getLocale() == $lang ? 'selected' : null }}>{{ $lang }}</option>
-		@endforeach
-	</select>
-</p>
-<span class="flag-icon flag-icon-en"></span>
-<span class="flag-icon flag-icon-ru"></span>
-<span class="flag-icon flag-icon-uk"></span>
+{{-- <p> --}}
+	{{-- <select id="select-language" name="language"> --}}
+		{{-- @foreach (config('languages.languages') as $lang) --}}
+			{{-- <option {{ \App::getLocale() == $lang ? 'selected' : null }}>{{ $lang }}</option> --}}
+		{{-- @endforeach --}}
+	{{-- </select> --}}
+<div class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+      Dropdown button
+    </a>
+    <div class="dropdown-menu">
+		<i class="dropdown-item" href="#">Link 1</i>
+		<i class="dropdown-item" href="#">Link 2</i>
+        <i class="dropdown-item" href="#">Link 3</i>
+    </div>
+</div>
+{{-- </p> --}}
+<a href="#"><span class="flag-icon flag-icon-en"></span></a>
+<a href="#"><span class="flag-icon flag-icon-ru"></span></a>
+<a href="#"><span class="flag-icon flag-icon-uk"></span></a>
+
+
+
 <script>
 // change language
-let selectLanguage = document.querySelector('#select-language');
-selectLanguage.onchange = function(e) {
-	let lang = this.value;
-	let route = "{{ route('setlocale') }}";
-	document.location.href = route + '/' + lang + '/';
-} 
+{{-- let selectLanguage = document.querySelector('#select-language'); --}}
+{{-- selectLanguage.onchange = function(e) { --}}
+	{{-- let lang = this.value; --}}
+	{{-- let route = "{{ route('setlocale') }}"; --}}
+	{{-- document.location.href = route + '/' + lang + '/'; --}}
+{{-- }  --}}
 </script>
