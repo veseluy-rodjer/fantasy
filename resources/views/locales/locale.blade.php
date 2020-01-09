6a83ad7e01de57eaba9e1e9e464added627b6d38
Nikolay <nikolay.m@filancy.com>
2020-01-09 10:03:56 +0200

    toggle language 4

--- resources/views/locales/locale.blade.php
+++ resources/views/locales/locale.blade.php
@@ -1,12 +1,13 @@
 <p>
 	<select id="select-language" name="language">
 		@foreach (config('languages.languages') as $lang)
-			<option {{ \App::getLocale() == $lang ? 'selected' : null }}><span class="flag-icon flag-icon-{{ $lang }}">{{ $lang }}</span></option>
+			<option {{ \App::getLocale() == $lang ? 'selected' : null }}>{{ $lang }}</option>
 		@endforeach
 	</select>
 </p>
-<span class="flag-icon flag-icon-{{ $lang }}"></span>
 <span class="flag-icon flag-icon-en"></span>
+<span class="flag-icon flag-icon-ru"></span>
+<span class="flag-icon flag-icon-uk"></span>
 <script>
 // change language
 let selectLanguage = document.querySelector('#select-language');
